2a9e6e33175c59173ac9b29665af72a27324f7db
yas-yokota <65379602+yas-yokota@users.noreply.github.com>
2024-06-04 17:58:39 +0900

    Create upper.js

---
+++ practice/chapter5/nodeSample/upper.js
@@ -0,0 +1,25 @@
+const http = require('node:http');
+var outLine = require('./upperMod');
+
+const hostname = '0.0.0.0';
+const port = 8081;
+const server = http.createServer();
+
+server.on("request", function (req, res) {
+        inLine = req.url;
+        switch (true) {
+          case /\/word\/.*/.test(inLine):
+            const regex = /^\/word\//;
+            let tmpLine = inLine.replace(regex, '')
+            res.writeHead(200, {"Content-Type": "test/plain"});
+            res.end(outLine.OutputJson(tmpLine));
+            break;
+          default:
+            console.log("not found");
+            break;
+        }
+});
+
+server.listen(port, hostname, () => {
+        console.log(`Server running at http://${hostname}:${port}`);
+});
